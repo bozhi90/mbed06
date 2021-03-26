9bf1aaffb94869cf62e7306c0bbd599e530a9d61
bozhi Tsai <bozhi9004@gmaill.com>
2021-03-26 07:36:14 -0700

    add hw1.cpp

---
+++ HW1/hw1.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+using namespace std::chrono;
+
+Ticker flipper;
+DigitalOut led3(LED3);
+
+void flip()
+{
+   led3 = !led3;
+}
+
+int main()
+{
+    led3 = 1;
+    flipper.attach(&flip, 500ms);
+    while(1);
+}
